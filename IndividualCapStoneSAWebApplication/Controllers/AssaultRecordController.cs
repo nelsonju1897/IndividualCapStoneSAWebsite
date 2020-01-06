c831813161c7c7838e8fa7ce739e33d27bad05cd
nelsonju1897 <nelsonju1897@gmail.com>
2020-01-06 13:07:04 -0600

    Completed the data visualization. Working on Trilio.

--- IndividualCapStoneSAWebApplication/Controllers/AssaultRecordController.cs
+++ IndividualCapStoneSAWebApplication/Controllers/AssaultRecordController.cs
@@ -104,5 +104,49 @@ namespace IndividualCapStoneSAWebApplication.Controllers
                 return View();
             }
         }
+
+        public ActionResult AssaultLocation()
+        {
+            ViewBag.AssaultLocation = db.AssaultRecords.Where(l => l.AssaultLocationId == 1).ToList().Count();
+            ViewBag.AssaultLocation2 = db.AssaultRecords.Where(l => l.AssaultLocationId == 2).ToList().Count();
+            ViewBag.AssaultLocation3 = db.AssaultRecords.Where(l => l.AssaultLocationId == 3).ToList().Count();
+            ViewBag.AssaultLocation4 = db.AssaultRecords.Where(l => l.AssaultLocationId == 4).ToList().Count();
+            ViewBag.AssaultLocation5 = db.AssaultRecords.Where(l => l.AssaultLocationId == 5).ToList().Count();
+            ViewBag.AssaultLocation6 = db.AssaultRecords.Where(l => l.AssaultLocationId == 6).ToList().Count();
+            ViewBag.AssaultLocation7 = db.AssaultRecords.Where(l => l.AssaultLocationId == 7).ToList().Count();
+
+            ViewBag.AlcoholAndOrDrug = db.AssaultRecords.Where(a => a.AlcoholAndOrDrugId == 1).ToList().Count();
+            ViewBag.AlcoholAndOrDrug2 = db.AssaultRecords.Where(a => a.AlcoholAndOrDrugId == 2).ToList().Count();
+            ViewBag.AlcoholAndOrDrug3 = db.AssaultRecords.Where(a => a.AlcoholAndOrDrugId == 3).ToList().Count();
+
+            ViewBag.AssaultType = db.AssaultRecords.Where(a => a.AssaultTypeId == 1).ToList().Count();
+            ViewBag.AssaultType2 = db.AssaultRecords.Where(a => a.AssaultTypeId == 2).ToList().Count();
+            ViewBag.AssaultType3 = db.AssaultRecords.Where(a => a.AssaultTypeId == 3).ToList().Count();
+            ViewBag.AssaultType4 = db.AssaultRecords.Where(a => a.AssaultTypeId == 4).ToList().Count();
+
+            ViewBag.Relationship = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 1).ToList().Count();
+            ViewBag.Relationship2 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 2).ToList().Count();
+            ViewBag.Relationship3 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 3).ToList().Count();
+            ViewBag.Relationship4 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 4).ToList().Count();
+            ViewBag.Relationship5 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 5).ToList().Count();
+            ViewBag.Relationship6 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 6).ToList().Count();
+            ViewBag.Relationship7 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 7).ToList().Count();
+            ViewBag.Relationship8 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 8).ToList().Count();
+            ViewBag.Relationship9 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 9).ToList().Count();
+            ViewBag.Relationship10 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 10).ToList().Count();
+            ViewBag.Relationship11 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 11).ToList().Count();
+            ViewBag.Relationship12 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 12).ToList().Count();
+            ViewBag.Relationship13 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 13).ToList().Count();
+            ViewBag.Relationship14 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 14).ToList().Count();
+            ViewBag.Relationship15 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 15).ToList().Count();
+            ViewBag.Relationship16 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 16).ToList().Count();
+            ViewBag.Relationship17 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 17).ToList().Count();
+            ViewBag.Relationship18 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 18).ToList().Count();
+            ViewBag.Relationship19 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 19).ToList().Count();
+            ViewBag.Relationship20 = db.AssaultRecords.Where(a => a.AttackerRelationshipId == 20).ToList().Count();
+
+            return View();
+
+        }
     }
 }
